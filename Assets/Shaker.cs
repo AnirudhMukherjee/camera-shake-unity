0660402e586d8d329a4483866e6c85ec848f8a47
AnirudhMukherjee <anirudhmk130@gmail.com>
2020-10-06 21:15:57 +0530

    init

---
+++ Assets/Shaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float timeElapsed = 0.0f;
+        
+
+        while (timeElapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+            //Generate random x,y values and shake the camera based on the same
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+            timeElapsed += Time.deltaTime;
+
+            //Wait for the next frame before running the loop once again
+            yield return null;
+
+        }
+        //Return to original Camera position
+        transform.localPosition = originalPos;
+    }
+}
---
+++ Assets/explosionTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosionTrigger : MonoBehaviour
+{
+
+    public Shaker sk;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            StartCoroutine(sk.Shake(.15f, .4f));
+        }
+    }
+}
